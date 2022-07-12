c1592374afbf63c74d4c7970024a98a6dc9786f4
Callum Brown <callum@calcuode.com>
2022-07-12 11:21:52 +0100

    Document Album.items() / LibModel.items() conflict
    Closes: https://github.com/beetbox/beets/issues/4404

--- beets/library.py
+++ beets/library.py
@@ -1150,6 +1150,9 @@ class Album(LibModel):
     def items(self):
         """Return an iterable over the items associated with this
         album.
+
+        This method predates the :meth:`LibModel.items` method inherited
+        from :meth:`beets.dbcore.Model.items`.
         """
         return self._db.items(dbcore.MatchQuery('album_id', self.id))
 
