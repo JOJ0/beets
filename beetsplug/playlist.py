07cc467e7e7d8be67d2f06862d289d6a5d44667a
J0J0 Todos <jojo@peek-a-boo.at>
2023-11-22 00:03:02 +0100

    Support m3u8 ending in playlist plugin quickfix

--- beetsplug/playlist.py
+++ beetsplug/playlist.py
@@ -46,8 +46,9 @@ class PlaylistQuery(InQuery[bytes]):
 
         paths = []
         for playlist_path in playlist_paths:
-            if not fnmatch.fnmatch(playlist_path, "*.[mM]3[uU]"):
+            if not fnmatch.fnmatch(playlist_path, "*.[mM]3[uU]?"):
                 # This is not am M3U playlist, skip this candidate
+                self._log.debug("This is not an m3u playlist")
                 continue
 
             try:
